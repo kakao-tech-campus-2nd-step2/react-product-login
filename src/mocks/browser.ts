1d76553db2fed8029922d3253d88728721144efb
cla6shade <cla6shade@gmail.com>
2024-07-23 14:22:53 +0900

    chore: 정답 코드 제거

--- src/mocks/browser.ts
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw';
-
-import { categoriesMockHandler } from '@/api/hooks/categories.mock';
-import { productsMockHandler } from '@/api/hooks/products.mock';
-
-export const worker = setupWorker(...categoriesMockHandler, ...productsMockHandler);
