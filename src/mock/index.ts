fa0435947cf1091167e0aa62a708a17e6ade02c0
cla6shade <cla6shade@gmail.com>
2024-07-24 01:02:49 +0900

    fix: 잘못된 intercept url 수정

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -3,9 +3,14 @@ import RequestURLs from '@constants/RequestURLs';
 import MockData from '@mock/MockData';
 import { setupWorker } from 'msw/browser';
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 const handlers = [
-  http.get(RequestURLs.CATEGORY, () => HttpResponse.json(MockData.MOCK_CATEGORIES)),
-  http.get(RequestURLs.CATEGORY_PRODUCTS, () => HttpResponse.json(MockData.MOCK_CATEGORY_ITEMS)),
+  http.get(BASE_URL + RequestURLs.CATEGORY, () => HttpResponse.json(MockData.MOCK_CATEGORIES)),
+  http.get(
+    BASE_URL + RequestURLs.CATEGORY_PRODUCTS,
+    () => HttpResponse.json(MockData.MOCK_CATEGORY_ITEMS),
+  ),
 ];
 
 const worker = setupWorker(...handlers);
