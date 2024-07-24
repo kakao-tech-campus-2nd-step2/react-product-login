8e68e59bdd35776c2cbfbf7a9f7e37b4507aab1d
JaeAnHan <jaean1999@naver.com>
2024-07-25 06:59:02 +0900

    feat: form의 validation 로직이 정상 동작하는지 확인

--- src/tests/order.test.tsx
+++ src/tests/order.test.tsx
@@ -7,7 +7,9 @@ import type { ReactElement } from 'react';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import { queryClient } from '@/api/instance';
+import { validateOrderForm } from '@/components/features/Order/OrderForm';
 import { OrderPage } from '@/pages/Order';
+import type { OrderFormData } from '@/types';
 import { orderHistorySessionStorage } from '@/utils/storage';
 
 import { worker } from '../../server';
@@ -69,3 +71,86 @@ test('오더테스트', async () => {
   expect(cashReceiptTypeSelect.value).toBe('PERSONAL');
   expect(cashReceiptNumberInput.value).toBe('1234567890');
 });
+
+describe('밸리데이션 검증', () => {
+  it('현금영수증을 요청할 경우 번호를 입력해주세요.', () => {
+    const orderData: OrderFormData = {
+      productId: 1,
+      productQuantity: 1,
+      messageCardTextMessage: '감사합니다!',
+      senderId: 123,
+      receiverId: 456,
+      hasCashReceipt: true,
+      cashReceiptNumber: '',
+    };
+    const result = validateOrderForm(orderData);
+
+    expect(result.isValid).toBe(false);
+
+    expect(result.errorMessage).toBe('현금영수증 번호를 입력해주세요.');
+  });
+
+  it('현금영수증 번호는 숫자만 입력해주세요.', () => {
+    const orderData: OrderFormData = {
+      ...baseOrderData,
+      hasCashReceipt: true,
+      cashReceiptNumber: 'abc123',
+    };
+    const result = validateOrderForm(orderData);
+
+    expect(result.isValid).toBe(false);
+
+    expect(result.errorMessage).toBe('현금영수증 번호는 숫자로만 입력해주세요.');
+  });
+
+  it('메시지를 입력해주세요.', () => {
+    const orderData: OrderFormData = {
+      ...baseOrderData,
+      messageCardTextMessage: '',
+    };
+    const result = validateOrderForm(orderData);
+
+    expect(result.isValid).toBe(false);
+
+    expect(result.errorMessage).toBe('메시지를 입력해주세요.');
+  });
+
+  it('메시지는 100자 이내로 입력해주세요.', () => {
+    const orderData: OrderFormData = {
+      ...baseOrderData,
+      messageCardTextMessage: 'ㅋ'.repeat(101),
+    };
+    const result = validateOrderForm(orderData);
+
+    expect(result.isValid).toBe(false);
+
+    expect(result.errorMessage).toBe('메시지는 100자 이내로 입력해주세요.');
+  });
+
+  it('통과해야 하는 테스트', () => {
+    const orderData: OrderFormData = {
+      productId: 1,
+      productQuantity: 2,
+      messageCardTextMessage: '정상 데이터',
+      senderId: 200,
+      receiverId: 300,
+      hasCashReceipt: true,
+      cashReceiptNumber: '1234567890',
+    };
+    const result = validateOrderForm(orderData);
+
+    expect(result.isValid).toBe(true);
+
+    expect(result.errorMessage).toBeUndefined();
+  });
+});
+
+const baseOrderData = {
+  productId: 1,
+  productQuantity: 2,
+  messageCardTextMessage: '감사합니다!',
+  senderId: 100,
+  receiverId: 200,
+  hasCashReceipt: false,
+  cashReceiptNumber: '123456',
+};
