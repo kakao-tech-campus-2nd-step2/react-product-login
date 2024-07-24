6e109e9a15e16a7c55bf08780f0e6e15e667215d
cla6shade <cla6shade@gmail.com>
2024-07-25 02:21:54 +0900

    chore: vitest configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -41,7 +41,7 @@ module.exports = {
     ],
     'import/no-extraneous-dependencies': [
       'error',
-      { devDependencies: ['**/mock/*'] },
+      { devDependencies: ['**/mock/*', '**/*.test.*'] },
     ],
   }
 }
