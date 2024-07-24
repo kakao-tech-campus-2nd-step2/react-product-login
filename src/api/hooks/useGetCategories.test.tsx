f8a03aca44a5cc3dbc59bed212eeb1d0cdf16c4e
Eric <real.dassy@gmail.com>
2024-07-25 08:01:30 +0900

    test: useGetCategories 훅 테스트 추가

---
+++ src/api/hooks/useGetCategories.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { renderHook, waitFor } from '@testing-library/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { server } from '@/mocks/server';
+import { useGetCategories } from '@/api/hooks/useGetCategorys';
+import { CATEGORIES_RESPONSE_DATA } from './categories.mock';
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const createWrapper = () => {
+  const queryClient = new QueryClient();
+
+  return ({ children }: { children: React.ReactNode }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+test('useGetCategories_카테고리_데이터_반환', async () => {
+  const { result } = renderHook(() => useGetCategories(), { wrapper: createWrapper() });
+
+  await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+  expect(result.current.data).toEqual(CATEGORIES_RESPONSE_DATA);
+});
