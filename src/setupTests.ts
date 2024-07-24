85d5b1fdac7c807e896bdd83d6893fe5bd053804
HyeonChae1104 <211063@jnu.ac.kr>
2024-07-24 13:30:45 +0900

    feat: 테스트 기반 환경을 구축

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+// 모든 테스트 전에 API mocking 설정
+beforeAll(() => server.listen());
+
+// 각 테스트 후 request 핸들러를 리셋
+afterEach(() => server.resetHandlers());
+
+// 모든 테스트 후 클린업
+afterAll(() => server.close());
