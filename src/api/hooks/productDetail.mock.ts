799ed8ab9205fee2416765ee76dd4a8a02e4abbe
yuni <hys990730@gmail.com>
2024-07-25 20:28:44 +0900

    feat: 제품 상세 정보 API 모킹 핸들러 추가
    - getProductDetailPath 함수 사용하여 제품 상세 정보 API 엔드포인트 정의
    - productDetailMockHandler를 통해 제품 상세 정보 API 요청 모킹
    - productId에 따라 응답 데이터 분기 처리
    - 존재하지 않는 productId에 대해 404 에러 응답 추가

---
+++ src/api/hooks/productDetail.mock.ts
@@ -0,0 +1,26 @@
+import { rest } from 'msw';
+
+import { getProductDetailPath } from './useGetProductDetail';
+
+// 제품 상세 정보 모킹 핸들러
+export const productDetailMockHandler = [
+  rest.get(getProductDetailPath(':productId'), (req, res, ctx) => {
+    const { productId } = req.params;
+
+    // 예시: productId에 따라 다른 데이터를 반환할 수 있습니다.
+    if (productId === '1') {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          id: 1,
+          name: '[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)',
+          price: 145000,
+          imageUrl:
+            'https://st.kakaocdn.net/product/gift/product/20240215083306_8e1db057580145829542463a84971ae3.png',
+          categoryId: 2920,
+        }),
+      );
+    }
+    return res(ctx.status(404), ctx.json({ error: 'Product not found' }));
+  }),
+];
