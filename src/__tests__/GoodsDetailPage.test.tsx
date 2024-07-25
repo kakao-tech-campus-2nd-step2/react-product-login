f3edc0276bfcb63ca4c3bdec8492c6b2a232bb45
seongikx <tjddlr3512@pusan.ac.kr>
2024-07-26 01:33:21 +0900

    Feat: 상품 상세 페이지 통합 테스트 코드 구현

---
+++ src/__tests__/GoodsDetailPage.test.tsx
@@ -0,0 +1,31 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+
+import { worker } from '@/mocks/server';
+import { GoodsDetailPage } from '@/pages/Goods/Detail';
+
+const queryClient = new QueryClient();
+
+beforeAll(() => worker.listen());
+afterEach(() => worker.resetHandlers());
+afterAll(() => worker.close());
+
+// TODO : 상품 상세 페이지 관련된 통합 테스트 코드
+// 상품 로딩 되는지
+
+test('상품 상세 정보를 표시해야 합니다', async () => {
+  render(
+    <QueryClientProvider client={queryClient}>
+      <MemoryRouter initialEntries={['/goods/3245119']}>
+        <Routes>
+          <Route path="/goods/:productId" element={<GoodsDetailPage />} />
+        </Routes>
+      </MemoryRouter>
+    </QueryClientProvider>,
+  );
+  await waitForElementToBeRemoved(() => screen.queryByRole('spinner'), { timeout: 5000 });
+  expect(
+    screen.getByText('[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)'),
+  ).toBeInTheDocument();
+});
