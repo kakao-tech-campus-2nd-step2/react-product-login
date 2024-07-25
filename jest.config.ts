1ae9e9664c62351ba4d03dfd2ae78bb0c48b03c2
Kuheewon <selly3109@naver.com>
2024-07-25 15:57:58 +0900

    error. cannot find module

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,17 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['./setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
 };
+
+export default config;
