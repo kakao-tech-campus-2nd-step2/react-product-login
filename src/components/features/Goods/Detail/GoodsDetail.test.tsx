3ca32a4be5c09442d4831566d15746ca8e8d99e7
Solbin Jung <jsolbin0724@gmail.com>
2024-07-25 15:24:23 +0900

    feat: 상품 상세 페이지 로그인 리디렉션 테스트

--- src/components/features/Goods/Detail/GoodsDetail.test.tsx
+++ src/components/features/Goods/Detail/GoodsDetail.test.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { setupServer } from 'msw/node';
 import React from 'react';
 import { MemoryRouter, Route, Routes, useNavigate } from 'react-router-dom';
@@ -9,6 +9,7 @@ import { productsMockHandler } from '@/api/hooks/products.mock';
 import { getDynamicPath } from '@/routes/path';
 
 import { GoodsDetail } from './index';
+import { OptionSection } from './OptionSection';
 
 const queryClient = new QueryClient();
 const mockServer = setupServer(...categoriesMockHandler, ...productsMockHandler);
@@ -33,6 +34,7 @@ const renderWithProviders = (ui: React.ReactElement) => {
       <MemoryRouter initialEntries={[`/products/${productId}`]}>
         <Routes>
           <Route path="/products/:productId" element={ui} />
+          <Route path="/login" element={<div>로그인</div>} />
           <Route path="/" element={<MockNavigate />} />
         </Routes>
       </MemoryRouter>
@@ -48,3 +50,20 @@ test('상품 상세 정보가 올바르게 표시되어야 한다', async () => 
   expect(screen.getByText('145000원')).toBeInTheDocument();
 });
 
+test('로그인 되지 않은 경우 로그인 페이지로 리디렉션되어야 한다', async () => {
+  const confirmMock = jest.spyOn(window, 'confirm').mockImplementation(() => true);
+
+  renderWithProviders(<OptionSection productId={productId} />);
+
+  const button = await screen.findByText('나에게 선물하기');
+
+  fireEvent.click(button);
+
+  expect(confirmMock).toHaveBeenCalledWith(
+    '로그인이 필요한 메뉴입니다.\n로그인 페이지로 이동하시겠습니까?'
+  );
+
+  expect(await screen.findByText('로그인')).toBeInTheDocument();
+
+  confirmMock.mockRestore();
+});
