806db970a59bd2a6882c2040eac5e3780dd3c046
loveydev <tjgus9650@gmail.com>
2024-07-25 15:53:29 +0900

    Feat: Jest 절대 경로를 위한 configure 추가

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,13 @@
 const path = require('path');
 
 module.exports = {
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+    },
+  },
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
