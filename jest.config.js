8b7c31e6b6da7bea4bddb11e56885d7909496c6d
Jungyoon Moon <halfmoonjy99@pusan.ac.kr>
2024-07-26 01:35:37 +0900

    feat: set unit test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'jest-transform-css',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(css|less|scss)$': 'jest-transform-css',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
