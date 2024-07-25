a28af344e93d961ccec3c6c1c29c287bfac657e3
cla6shade <cla6shade@gmail.com>
2024-07-25 16:09:31 +0900

    test: 테스트 코드 영어로 통일
    영어로 적어보고 싶었어요.,.

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -3,8 +3,8 @@ import {
 } from 'vitest';
 import { isNumericString } from '@utils/index';
 
-describe('Numeric string test', () => {
-  it('asdfasdf는 numeric string이 아니어야 합니다.', () => {
+describe('Util function test', () => {
+  it('The string `asdfasdf` should not be numeric', () => {
     const str = 'asdfasdf';
     expect(isNumericString(str)).toBe(false);
   });
