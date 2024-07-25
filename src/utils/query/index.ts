efb7e4a22355835516d29a51e1a6de25e77423dc
cla6shade <cla6shade@gmail.com>
2024-07-26 03:53:43 +0900

    feat: 리퀘스트 유틸 함수

--- src/utils/query/index.ts
+++ src/utils/query/index.ts
@@ -1,11 +1,20 @@
 import { axiosInstance, replacePathParams } from '@utils/network';
 import RequestURLs from '@constants/RequestURLs';
+import { LoginRequestBody } from '@/types/request';
 import {
   CategoryResponse,
+  LoginResponse,
   ProductDetailResponse,
 } from '@/types/response';
 import { CategoryRepository } from '@/types';
 import { ProductData } from '@/dto';
+
+export const requestAuth = async (body: LoginRequestBody, authType: 'login' | 'register') => {
+  const url = authType === 'register' ? RequestURLs.REGISTER : RequestURLs.LOGIN;
+  const response = await axiosInstance.post<LoginResponse>(url, body);
+
+  return response.data;
+};
 
 export const fetchCategories = async () => {
   const response = await axiosInstance.get<CategoryResponse>(RequestURLs.CATEGORY);
