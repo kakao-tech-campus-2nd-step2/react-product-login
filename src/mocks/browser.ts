e13b0c02bef2d1e1e16865a8da5e78750f9ebdbe
yuni <hys990730@gmail.com>
2024-07-25 20:11:08 +0900

    feat: MSW를 사용한 상세 및 옵션 API 모킹 핸들러 추가
    - 상세 API 관련 productDetail.mock.ts 파일 추가
      - 제품 상세 정보를 모킹하는 핸들러 정의
    - 옵션 API 관련 productOptions.mock.ts 파일 추가
      - 제품 옵션 정보를 모킹하는 핸들러 정의
    - MSW 설정 파일 setupMockWorker.ts 업데이트
      - productDetail.mock.ts와 productOptions.mock.ts 핸들러를 MSW 설정에 추가
    - 기존 categoriesMockHandler와 productsMockHandler와 함께 worker 설정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,5 +2,12 @@ import { setupWorker } from 'msw';
 
 import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
+import { productDetailMockHandler } from '@/api/hooks/productDetail.mock';
+import { productOptionsMockHandler } from './productOptions.mock';
 
-export const worker = setupWorker(...categoriesMockHandler, ...productsMockHandler);
+export const worker = setupWorker(
+  ...categoriesMockHandler, 
+  ...productsMockHandler, 
+  ...productDetailMockHandler, 
+  ...productOptionsMockHandler
+);
