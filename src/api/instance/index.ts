f644fceecfefff26a34c0b241a63b396cbbb4b39
박건규 <geongyu09@gmail.com>
2024-07-26 22:16:41 +0900

    feat: 토큰을 담는 axios intance 구현

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -2,6 +2,8 @@ import { QueryClient } from '@tanstack/react-query';
 import type { AxiosInstance, AxiosRequestConfig } from 'axios';
 import axios from 'axios';
 
+import { authSessionStorage } from '@/utils/storage';
+
 const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
   const instance = axios.create({
     timeout: 5000,
@@ -19,7 +21,15 @@ const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
 export const BASE_URL = 'https://api.example.com';
 // TODO: 추후 서버 API 주소 변경 필요
 export const fetchInstance = initInstance({
-  baseURL: 'https://api.example.com',
+  baseURL: BASE_URL,
+});
+
+const token = authSessionStorage.get();
+export const fetchWithToken = initInstance({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
 });
 
 export const queryClient = new QueryClient({
