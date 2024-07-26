6f21fe086903f1a56c37c5393645bb18717d8703
Kuheewon <selly3109@naver.com>
2024-07-26 15:59:45 +0900

    feat. 회원 가입하면 자동으로 로그인 되도록함

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { Spacing } from '@/components/common/layouts/Spacing';
 import { AiDiscoveryBanner } from '@/components/features/Home/AiDiscoveryBanner';
 import { CategorySection } from '@/components/features/Home/CategorySection';
 import { SelectFriendsBanner } from '@/components/features/Home/SelectFriendsBanner';
+import { authSessionStorage } from '@/utils/storage';
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+  const user = authSessionStorage.get();
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [user, navigate]);
+
   return (
     <>
       <SelectFriendsBanner />
