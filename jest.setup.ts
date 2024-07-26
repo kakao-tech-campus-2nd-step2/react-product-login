1042f595194faffb9abc6db3e651ffa80b57437e
ajin <aj5210@naver.com>
2024-07-26 19:57:16 +0900

    chore: test env 설정 추가

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,9 +1,11 @@
 import React from 'react';
 
 import '@testing-library/jest-dom';
+import dotenv from 'dotenv';
 
 import { server } from './src/mocks/server';
 
+dotenv.config({ path: '.env.test' });
 global.React = React;
 
 beforeAll(() => server.listen());
