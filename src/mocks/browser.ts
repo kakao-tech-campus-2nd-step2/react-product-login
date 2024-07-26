6d4bd55a5643f0b93b2cd9955a4f4e0940b3beb0
Dobbymin <gmin990624@gmail.com>
2024-07-26 21:33:45 +0900

    feat(mock): mock-data handler setting

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,6 +1,18 @@
 import { setupWorker } from 'msw';
 
 import { categoriesMockHandler } from '@/api/hooks/categories.mock';
+import { loginMockHandler } from '@/api/hooks/login.mock';
+import { productOptionsMockHandler } from '@/api/hooks/product-options.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
+import { registerMockHandler } from '@/api/hooks/register.mock';
+import { getWishListMockHandler } from '@/api/hooks/wishList.mock';
 
-export const worker = setupWorker(...categoriesMockHandler, ...productsMockHandler);
+export const worker = setupWorker(
+  ...categoriesMockHandler,
+  ...productsMockHandler,
+  ...registerMockHandler,
+  ...loginMockHandler,
+  ...registerMockHandler,
+  ...productOptionsMockHandler,
+  ...getWishListMockHandler,
+);
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,6 +1,18 @@
 import { setupServer } from 'msw/node';
 
 import { categoriesMockHandler } from '@/api/hooks/categories.mock';
+import { loginMockHandler } from '@/api/hooks/login.mock';
+import { productOptionsMockHandler } from '@/api/hooks/product-options.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
+import { registerMockHandler } from '@/api/hooks/register.mock';
+import { getWishListMockHandler } from '@/api/hooks/wishList.mock';
 
-export const server = setupServer(...categoriesMockHandler, ...productsMockHandler);
+export const server = setupServer(
+  ...categoriesMockHandler,
+  ...productsMockHandler,
+  ...registerMockHandler,
+  ...loginMockHandler,
+  ...registerMockHandler,
+  ...productOptionsMockHandler,
+  ...getWishListMockHandler,
+);
