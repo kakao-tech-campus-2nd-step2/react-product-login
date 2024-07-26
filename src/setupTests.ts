c9ecc5b10abd50e41d4527c21cc541a15a728b59
yunseong0404 <333665@naver.com>
2024-07-26 09:45:30 +0900

    chore: react testing libray 설정

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+//각 테스트가 DOM에 렌더링해놓은 내용들을 테스트가 끝날 때 다음 테스트를 위해서 지워주는 코드
+import '@testing-library/react/cleanup-after-each';
+//jest-dom가 제공하는 matcher를 Jest 테스트 러너에게 인식시키는 코드
+import '@testing-library/jest-dom/extend-expect';
