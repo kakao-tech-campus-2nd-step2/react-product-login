cbed542dc07009a770d5f3c444e154bf2baf369d
Eric <real.dassy@gmail.com>
2024-07-26 16:00:41 +0900

    refactor: MSW 설정 파일 추가

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,6 +1,13 @@
 import { setupWorker } from 'msw';
 
+import { authHandlers } from '@/api/hooks/authHandlers.mock';
 import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
+import { wishlistHandler } from '@/api/hooks/wishlistHandler.mock';
 
-export const worker = setupWorker(...categoriesMockHandler, ...productsMockHandler);
+export const worker = setupWorker(
+  ...categoriesMockHandler,
+  ...productsMockHandler,
+  ...authHandlers,
+  ...wishlistHandler,
+);
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,13 @@
 import { setupServer } from 'msw/node';
+
+import { authHandlers } from '@/api/hooks/authHandlers.mock';
 import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
+import { wishlistHandler } from '@/api/hooks/wishlistHandler.mock';
 
-export const server = setupServer(...categoriesMockHandler, ...productsMockHandler);
+export const server = setupServer(
+  ...categoriesMockHandler,
+  ...productsMockHandler,
+  ...authHandlers,
+  ...wishlistHandler,
+);
