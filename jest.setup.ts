8d5f275f914c7cc7edabe9cdf22194da73910cc2
ajin <aj5210@naver.com>
2024-07-26 14:32:29 +0900

    chore: 테스트 환경 설정 수정
    - global react 추가
    - module directories 설정 추가
    - tsconfig include 옵션 수정

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,10 @@
+import React from 'react';
+
 import '@testing-library/jest-dom';
 
 import { server } from './src/mocks/server';
+
+global.React = React;
 
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
 afterEach(() => server.resetHandlers());
