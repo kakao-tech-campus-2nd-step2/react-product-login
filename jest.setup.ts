3b36a64f30adb0071c30698908d7e883a65f7b3c
ajin <aj5210@naver.com>
2024-07-26 17:07:35 +0900

    test: product detail page 통합 테스트 추가

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,6 +6,6 @@ import { server } from './src/mocks/server';
 
 global.React = React;
 
-beforeAll(() => server.listen({ onUnhandledRequest: 'bypass' }));
+beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
