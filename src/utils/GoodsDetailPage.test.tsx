f49ce62e117cb9ec540a75a48184ff2ae39de152
diwoni <jgw117@naver.com>
2024-07-26 12:05:38 +0900

    test: 상품 상세 페이지 통합테스트 작성(옵션)

--- src/utils/GoodsDetailPage.test.tsx
+++ src/utils/GoodsDetailPage.test.tsx
@@ -1,12 +1,13 @@
 import '@testing-library/jest-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import type { ReactNode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { PRODUCTS_MOCK_DATA } from '@/api/hooks/products.mock';
 import { GoodsDetail } from '@/components/features/Goods/Detail';
+import { OptionSection } from '@/components/features/Goods/Detail/OptionSection';
 import { AuthProvider } from '@/provider/Auth';
 
 const queryClient = new QueryClient();
@@ -51,6 +52,32 @@ describe('GoodsDetail 컴포넌트', () => {
     await waitFor(() => {
       const imgElement = screen.getByRole('img');
       expect(imgElement).toHaveAttribute('src', imageUrl);
+    });
+  });
+  it('옵션 정보를 올바르게 표시해야 한다.', async () => {
+    const product = PRODUCTS_MOCK_DATA.content[0];
+    const productId = product.id.toString();
+
+    renderWithProviders(<OptionSection productId={productId} />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Option A')).toBeInTheDocument();
+    });
+  });
+
+  it('옵션을 선택하고 총 결제 금액을 올바르게 표시해야 한다.', async () => {
+    const product = PRODUCTS_MOCK_DATA.content[0];
+    const productId = product.id.toString();
+    const { price } = product;
+
+    renderWithProviders(<OptionSection productId={productId} />);
+
+    const countOption = screen.getByRole('spinbutton');
+    fireEvent.change(countOption, { target: { value: '2' } });
+
+    await waitFor(() => {
+      expect(screen.getByText(`총 결제 금액`)).toBeInTheDocument();
+      expect(screen.getByText(`${price * 2}원`)).toBeInTheDocument();
     });
   });
 });
