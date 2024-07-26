ba5f6a97c51036442fb485a5cd5a50ed1297fae6
JaeAnHan <jaean1999@naver.com>
2024-07-26 17:40:23 +0900

    refactor: encapsulate handling response token from UI component

--- src/api/user/api.ts
+++ src/api/user/api.ts
@@ -3,6 +3,9 @@ import { fetchInstance } from '@/api/instance';
 export const registerUser = async (email: string, password: string) => {
   try {
     const response = await fetchInstance.post('/api/members/register', { email, password });
+    const { token } = response.data;
+    localStorage.setItem('token', token);
+
     return response.data;
   } catch (error) {
     throw new Error('회원가입에 실패했습니다.');
