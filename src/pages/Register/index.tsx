ebf98f34e7f1a4cf804005cda175bd678c63aac0
faddishcorn <juni129461@gmail.com>
2024-07-26 14:53:53 +0900

    feat: 회원가입 페이지 구현

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,90 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import KAKAO_LOGO from '@/assets/kakao_logo.svg';
+import { Button } from '@/components/common/Button';
+import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
+import { Spacing } from '@/components/common/layouts/Spacing';
+import { breakpoints } from '@/styles/variants';
+import { authSessionStorage } from '@/utils/storage';
+
+export const RegisterPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleRegister = async () => {
+    if (!email || !password) {
+      alert('이메일과 비밀번호를 입력해주세요.');
+      return;
+    }
+
+    try {
+      const response = await fetch('https://api.example.com/api/members/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        authSessionStorage.set(data.token);
+        localStorage.setItem('user', JSON.stringify({ email: data.email, token: data.token }));
+
+        navigate('/');
+        window.location.reload();
+      } else {
+        alert('회원가입 실패. 다시 시도해주세요.');
+      }
+    } catch (error) {
+      console.error('회원가입 요청 중 오류 발생:', error);
+      alert('회원가입 요청 중 오류가 발생했습니다.');
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Logo src={KAKAO_LOGO} alt="카카오 CI" />
+      <FormWrapper>
+        <UnderlineTextField placeholder="이메일" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <Spacing />
+        <UnderlineTextField
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Spacing height={{ initial: 40, sm: 60 }} />
+        <Button onClick={handleRegister}>회원가입</Button>
+        <Spacing height={20} />
+        <Button onClick={() => navigate('/login')}>로그인</Button>
+      </FormWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Logo = styled.img`
+  width: 88px;
+  color: #333;
+`;
+
+const FormWrapper = styled.article`
+  width: 100%;
+  max-width: 580px;
+  padding: 16px;
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    padding: 60px 52px;
+  }
+`;
