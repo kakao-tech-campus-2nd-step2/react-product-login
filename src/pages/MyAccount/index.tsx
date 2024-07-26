c1d870f4724c8eab4e5b485a07dbfe62e0c8ed1c
Kuheewon <selly3109@naver.com>
2024-07-26 19:04:36 +0900

    feat. 관심 목록 리스트 생성

--- src/pages/MyAccount/index.tsx
+++ src/pages/MyAccount/index.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
 
 import { Button } from '@/components/common/Button';
 import { Spacing } from '@/components/common/layouts/Spacing';
@@ -8,6 +9,7 @@ import { authSessionStorage } from '@/utils/storage';
 
 export const MyAccountPage = () => {
   const authInfo = useAuth();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     authSessionStorage.set(undefined);
@@ -16,19 +18,18 @@ export const MyAccountPage = () => {
     window.location.replace(redirectURL);
   };
 
+  const handleFavorites = () => {
+    navigate('/favorites'); // 관심 목록 페이지로 이동
+  };
+
   return (
     <Wrapper>
-      {authInfo?.name}님 안녕하세요! <Spacing height={64} />
-      <Button
-        size="small"
-        theme="darkGray"
-        onClick={handleLogout}
-        style={{
-          maxWidth: '200px',
-        }}
-      >
-        로그아웃
-      </Button>
+      {authInfo?.name}님 안녕하세요!
+      <Spacing height={64} />
+      <ButtonContainer>
+        <StyledButton onClick={handleLogout}>로그아웃</StyledButton>
+        <StyledButton onClick={handleFavorites}>관심 목록</StyledButton>
+      </ButtonContainer>
     </Wrapper>
   );
 };
@@ -36,11 +37,22 @@ export const MyAccountPage = () => {
 const Wrapper = styled.div`
   width: 100%;
   padding: 80px 0 120px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   font-weight: 700;
   font-size: 36px;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px; /* 버튼 간격 조정 */
+`;
+
+const StyledButton = styled(Button)`
+  width: 200px;
+  height: 50px; /* 버튼 높이 조정 */
+  font-size: 16px; /* 폰트 크기 조정 */
 `;
