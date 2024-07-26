714af4bed5cb35440516f8546c6501cdacf0621f
dlawlghks <ikuwlghks@naver.com>
2024-07-26 17:53:56 +0900

    refactor: LoginPage 컴포넌트 로그인 로직 및 회원가입 버튼 추가

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import KAKAO_LOGO from '@/assets/kakao_logo.svg';
 import { Button } from '@/components/common/Button';
@@ -12,28 +12,45 @@ import { authSessionStorage } from '@/utils/storage';
 export const LoginPage = () => {
   const [id, setId] = useState('');
   const [password, setPassword] = useState('');
-  const [queryParams] = useSearchParams();
+  const [] = useSearchParams();
+  const navigate = useNavigate();
 
-  const handleConfirm = () => {
+  const handleLogin = () => {
     if (!id || !password) {
       alert('아이디와 비밀번호를 입력해주세요.');
       return;
     }
 
-    // TODO: API 연동
+    const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
+    const user = storedUsers.find(
+      // eslint-disable-next-line @typescript-eslint/no-shadow
+      (user: { id: string; password: string }) => user.id === id && user.password === password,
+    );
 
-    // TODO: API 연동 전까지 임시 로그인 처리
-    authSessionStorage.set(id);
+    if (user) {
+      const mockToken = 'mock-token';
+      authSessionStorage.set(mockToken); // 로그인 시 token 저장
+      localStorage.setItem('user', JSON.stringify(user)); // 유저 정보 저장
+      alert('로그인 성공');
+      window.location.replace(`${window.location.origin}/`); // 메인 페이지로 리디렉션
+    } else {
+      alert('아이디 또는 비밀번호가 잘못되었습니다.');
+    }
+  };
 
-    const redirectUrl = queryParams.get('redirect') ?? `${window.location.origin}/`;
-    return window.location.replace(redirectUrl);
+  const handleSignup = () => {
+    navigate('/signup');
   };
 
   return (
     <Wrapper>
       <Logo src={KAKAO_LOGO} alt="카카고 CI" />
       <FormWrapper>
-        <UnderlineTextField placeholder="이름" value={id} onChange={(e) => setId(e.target.value)} />
+        <UnderlineTextField
+          placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        />
         <Spacing />
         <UnderlineTextField
           type="password"
@@ -41,14 +58,12 @@ export const LoginPage = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
-        <Spacing
-          height={{
-            initial: 40,
-            sm: 60,
-          }}
-        />
-        <Button onClick={handleConfirm}>로그인</Button>
+        <Spacing height={{ initial: 40, sm: 60 }} />
+        <Button onClick={handleLogin}>로그인</Button>
+        <Spacing height={20} />
+        <Button theme="outline" onClick={handleSignup}>
+          회원가입
+        </Button>
       </FormWrapper>
     </Wrapper>
   );
