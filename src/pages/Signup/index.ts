526e094a34148a7d3ab7bbc983d757294d8eb93e
yuni <hys990730@gmail.com>
2024-07-26 12:21:59 +0900

    feat(router): 회원가입 페이지 라우팅 설정 및 컴포넌트 추가

---
+++ src/pages/Signup/index.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const SignupPage = () => {
+  return (
+    <Wrapper>
+      <h1>회원가입 페이지</h1>
+      {/* 회원가입 폼 내용을 여기에 추가합니다 */}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
