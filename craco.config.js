b205ddc40dea7c4b5647edc1717f594fd9a6264b
joshuadesu <jeongwongwak@naver.com>
2024-07-26 14:11:52 +0900

    Chore: Add Jest 설정(Axios && 절대경로)

--- craco.config.js
+++ craco.config.js
@@ -6,4 +6,15 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+
+  // https://jestjs.io/docs/configuration#modulenamemapper-objectstring-string--arraystring 참고
+  // 해당 부분은 방법이 떠오르지 않아 구글링을 통해 해결하였습니다.
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+        // https://stackoverflow.com/questions/73958968/cannot-use-import-statement-outside-a-module-with-axios #23 답변
+        "^axios$": "axios/dist/node/axios.cjs",
+      },},
+  },
 };
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+}
