5c03baa1f79bd4897a8418056dabde997f29b41d
eunjin <eunjin0321@naver.com>
2024-07-26 19:13:45 +0900

    feat : mock 데이터 사용을 위한 server 추가

---
+++ src/mocks/server.ts
@@ -0,0 +1,5 @@
+import { setupServer } from 'msw/node';
+import {categoriesMockHandler} from '@/api/hooks/categories.mock';
+import {productsMockHandler} from '@/api/hooks/products.mock';
+
+export const server = setupServer(...categoriesMockHandler, ...productsMockHandler);
