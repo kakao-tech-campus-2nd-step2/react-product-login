a2516a7849cbd305d1896cd6de0a09a7a0041094
ajin <aj5210@naver.com>
2024-07-26 14:39:05 +0900

    test: 현금 영수증 여부 테스트 코드 추가

--- src/__test__/OrderForm.test.tsx
+++ src/__test__/OrderForm.test.tsx
@@ -1,38 +1,23 @@
-import { ReactElement, ReactNode } from 'react';
-import { FormProvider, useForm } from 'react-hook-form';
-import { BrowserRouter } from 'react-router-dom';
+import { ReactElement } from 'react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 
 import { ChakraProvider } from '@chakra-ui/react';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { QueryClientProvider } from '@tanstack/react-query';
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { queryClient } from '@/api/config/queryClient';
 import { OrderForm } from '@/pages/OrderPage/components/OrderForm';
-import { OrderForm as OrderFormType } from '@/pages/OrderPage/hooks/useOrderForm';
-import { OrderSchema, OrderValidationErrorMessages } from '@/schema/index';
 import { OrderHistory } from '@/types/orderType';
 
-const Form = ({ children }: { children: ReactNode }) => {
-  const form = useForm<OrderFormType>({
-    resolver: zodResolver(OrderSchema),
-    defaultValues: {
-      gitfMessage: '',
-      isCashChecked: false,
-      cashReceiptNumber: '',
-    },
-  });
-
-  return <FormProvider {...form}>{children} </FormProvider>;
-};
-
-const renderWithProviders = (ui: ReactElement) => {
+const renderWithProviders = (ui: ReactElement, { route = '/' } = {}) => {
   return render(
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Form>{ui}</Form>
-        </BrowserRouter>
+        <MemoryRouter initialEntries={[route]}>
+          <Routes>
+            <Route path="/" element={ui} />
+          </Routes>
+        </MemoryRouter>
       </QueryClientProvider>
     </ChakraProvider>
   );
@@ -43,116 +28,11 @@ const orderHistory: OrderHistory = {
   productQuantity: 1,
 };
 
-const errorMessage = OrderValidationErrorMessages;
+describe('OrderForm의 유효성 테스트', () => {
+  test('렌더링 확인', async () => {
+    renderWithProviders(<OrderForm orderHistory={orderHistory} />);
 
-describe('OrderForm을 제출했을 때 form field 유효성 검사', () => {
-  beforeEach(() => {
-    window.alert = jest.fn();
-  });
-
-  it('gitfMessage가 입력되지 않으면 에러 메세지를 보여준다. ', async () => {
-    await act(async () => {
-      renderWithProviders(<OrderForm orderHistory={orderHistory} />);
-    });
-
-    const giftMessageTextField = screen.getByTestId('gift-message-field');
-    expect(giftMessageTextField).toBeInTheDocument();
-    expect(giftMessageTextField).toHaveTextContent('');
-
-    const submitButton = screen.getByTestId('submit-button');
-    expect(submitButton).toBeInTheDocument();
-    fireEvent.click(submitButton);
-
-    const message = await screen.findByText(errorMessage.giftMessageRequired);
-    expect(message).toBeInTheDocument();
-  });
-
-  it('gitfMessage가 100자가 넘어가면 에러 메세지를 보여준다. ', async () => {
-    await act(async () => {
-      renderWithProviders(<OrderForm orderHistory={orderHistory} />);
-    });
-
-    const giftMessageTextField = screen.getByTestId('gift-message-field');
-    expect(giftMessageTextField).toBeInTheDocument();
-    fireEvent.change(giftMessageTextField, {
-      target: { value: 't'.repeat(101) },
-    });
-
-    const submitButton = screen.getByTestId('submit-button');
-    expect(submitButton).toBeInTheDocument();
-    fireEvent.click(submitButton);
-
-    const message = await screen.findByText(errorMessage.giftMessageTooLong);
-    expect(message).toBeInTheDocument();
-  });
-
-  it('현금 영수증을 체크하고 번호를 입력하지 않으면 에러 메세지를 보여준다. ', async () => {
-    await act(async () => {
-      renderWithProviders(<OrderForm orderHistory={orderHistory} />);
-    });
-
-    const giftMessageTextField = screen.getByTestId('gift-message-field');
-    expect(giftMessageTextField).toBeInTheDocument();
-    fireEvent.change(giftMessageTextField, {
-      target: { value: 'test' },
-    });
-
-    const cashCheckedField = screen.getByTestId(
-      'cash-checked-field'
-    ) as HTMLInputElement;
-    expect(cashCheckedField).toBeInTheDocument();
-    fireEvent.click(cashCheckedField);
-    expect(cashCheckedField.checked).toBe(true);
-
-    const cashNumberField = screen.queryByTestId(
-      'cash-number-field'
-    ) as HTMLInputElement;
-    expect(cashNumberField).toBeInTheDocument();
-    expect(cashNumberField).toHaveTextContent('');
-
-    const submitButton = screen.getByTestId('submit-button');
-    expect(submitButton).toBeInTheDocument();
-    fireEvent.click(submitButton);
-
-    const message = await screen.findByText(
-      errorMessage.cashReceiptNumberRequired
-    );
-    expect(message).toBeInTheDocument();
-  });
-
-  it('현금 영수증을 체크하고 - 없이 번호를 정확히 입력하지 않으면 에러 메세지를 보여준다. ', async () => {
-    await act(async () => {
-      renderWithProviders(<OrderForm orderHistory={orderHistory} />);
-    });
-
-    const giftMessageTextField = screen.getByTestId('gift-message-field');
-    expect(giftMessageTextField).toBeInTheDocument();
-    fireEvent.change(giftMessageTextField, {
-      target: { value: 'test' },
-    });
-
-    const cashCheckedField = screen.getByTestId(
-      'cash-checked-field'
-    ) as HTMLInputElement;
-    expect(cashCheckedField).toBeInTheDocument();
-    fireEvent.click(cashCheckedField);
-    expect(cashCheckedField.checked).toBe(true);
-
-    const cashNumberField = screen.queryByTestId(
-      'cash-number-field'
-    ) as HTMLInputElement;
-    expect(cashNumberField).toBeInTheDocument();
-    fireEvent.change(cashNumberField, {
-      target: { value: '010-1234-5678' },
-    });
-
-    const submitButton = screen.getByTestId('submit-button');
-    expect(submitButton).toBeInTheDocument();
-    fireEvent.click(submitButton);
-
-    const message = await screen.findByText(
-      errorMessage.cashReceiptNumberInvalid
-    );
-    expect(message).toBeInTheDocument();
+    const cashReceiptCheckbox = screen.getByRole('checkbox');
+    expect(cashReceiptCheckbox).toBeInTheDocument();
   });
 });
