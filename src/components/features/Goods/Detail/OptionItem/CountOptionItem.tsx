fe83ec419898daf3ef7686ff879e88e3a1454453
hojeong26 <rkdghwjd0926@jnu.ac.kr>
2024-07-27 20:50:57 +0900

    feat: 상품 상세 페이지에 관심 등록 버튼 추가

--- src/components/features/Goods/Detail/OptionItem/CountOptionItem.tsx
+++ src/components/features/Goods/Detail/OptionItem/CountOptionItem.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { AddIcon, MinusIcon } from '@chakra-ui/icons';
 import { IconButton, Input, useNumberInput } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { HeartOutlined, HeartFilled } from '@ant-design/icons';
 
 type Props = {
   name: string;
@@ -26,14 +28,34 @@ export const CountOptionItem = ({
       onChange(valueAsString);
     },
   });
+  const [isChecked, setIsChecked] = useState(false);
 
   const increment = getIncrementButtonProps();
   const decrement = getDecrementButtonProps();
   const input = getInputProps();
 
+  const wishhandler = () => {
+    setIsChecked((prev) => {
+      const newChecked = !prev;
+      if (newChecked) {
+        alert('관심 등록 완료');
+      }
+      return newChecked;
+    });
+  };
+
   return (
     <Wrapper>
-      <Title>{name}</Title>
+      <div style={{ display: 'flex' }}>
+        <Title>{name}</Title>
+        <div className="icons-list">
+          {isChecked ? (
+            <HeartFilled style={{ color: 'red', fontSize: '20px' }} onClick={wishhandler} />
+          ) : (
+            <HeartOutlined style={{ fontSize: '20px' }} onClick={wishhandler} />
+          )}
+        </div>
+      </div>
       <InputWrapper>
         <IconButton {...decrement} aria-label="수량 1개 감소" icon={<MinusIcon />} />
         <Input {...input} />
@@ -56,6 +78,7 @@ const Title = styled.p`
   color: #111;
   word-wrap: break-word;
   word-break: break-all;
+  margin-right: 10px;
 `;
 
 const InputWrapper = styled.div`
