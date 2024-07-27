8676138157bf2500433768969eb6681dc399fc64
JeongSeoyun <xeonj@naver.com>
2024-07-28 00:58:16 +0900

    chore: jest 경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,15 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env', // 최신 JavaScript 문법을 지원
-      '@babel/preset-react', // React JSX 문법을 지원
-      '@babel/preset-typescript' // TypeScript 문법을 지원
-    ],
-    plugins: [
-      ['module-resolver', {
-        root: ['./src'],
-        alias: {
-          '@': './src', // '@'를 'src'로 매핑
-        },
-      }],
-    ],
-  };
-  
+  presets: [
+    '@babel/preset-env', // 최신 JavaScript 문법을 지원
+    '@babel/preset-react', // React JSX 문법을 지원
+    '@babel/preset-typescript' // TypeScript 문법을 지원
+  ],
+  plugins: [
+    ['module-resolver', {
+      root: ['./src'],
+      alias: {
+        '@': './src', // '@'를 'src'로 매핑
+      },
+    }],
+  ],
+};
--- craco.config.js
+++ craco.config.js
@@ -4,20 +4,27 @@ module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+      '@src': path.resolve(__dirname, 'src'),
     },
   },
   jest: {
-    babel: {
-      addPresets: true /* (default value) */,
-      addPlugins: true /* (default value) */,
-    },
-    configure: { /* ... */ },
     configure: (jestConfig, { env, paths, resolve, rootDir }) => {
-      jestConfig.moduleNameMapper = {
-        ...jestConfig.moduleNameMapper,
-        '^axios$': 'axios/dist/node/axios.cjs',
+      // Babel을 사용하여 JS 및 TS 파일을 변환하도록 설정
+      jestConfig.transform = {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
       };
-      
+
+      // 모듈 경로를 매핑
+      jestConfig.moduleNameMapper = {
+        '^@/(.*)$': '<rootDir>/src/$1',
+        '^@src/(.*)$': '<rootDir>/src/$1',
+      };
+
+      // transformIgnorePatterns 설정을 추가하여 필요한 모듈에 대해서만 변환을 수행하도록 설정
+      jestConfig.transformIgnorePatterns = [
+        '/node_modules/(?!(axios|@tanstack|react-query)/)', // 여기서 필요한 모듈들을 나열
+      ];
+
       return jestConfig;
     },
   },
