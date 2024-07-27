69aa4f34427fe42072efb5c96a65f1d98547de57
unknown <kc21ynje@gmail.com>
2024-07-27 16:04:18 +0900

    feat: add form validation test

--- src/__tests__/OrderForm.test.tsx
+++ src/__tests__/OrderForm.test.tsx
@@ -1,8 +1,9 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { FormProvider, useForm } from "react-hook-form";
 
+import { OrderForm } from "@/components/features/Order/OrderForm";
 import { CashReceiptFields } from "@/components/features/Order/OrderForm/Fields/CashReceiptFields";
 import { MessageCardFields } from "@/components/features/Order/OrderForm/Fields/MessageCardFields";
 
@@ -50,6 +51,14 @@ const getCashReceiptNumber = () => {
 const getMessage = () => {
   return screen.getByPlaceholderText("선물과 함께 보낼 메시지를 적어보세요");
 };
+
+const getSubmit = () => {
+  return screen.getByText(/결제하기/i);
+};
+
+beforeAll(() => {
+  jest.spyOn(window, "alert").mockImplementation(() => {});
+});
 
 describe("Order Page", () => {
   describe("CashReceiptFields", () => {
@@ -105,6 +114,75 @@ describe("Order Page", () => {
       fireEvent.change(message, { target: { value: "메시지" } });
 
       expect(message).toHaveValue("메시지");
+    });
+  });
+
+  describe("Form Validation", () => {
+    beforeEach(async () => {
+      await act(async () => {
+        renderWithOrderWrapper(<OrderForm orderHistory={{ id: 1, count: 1 }} />);
+      });
+    });
+
+    test("현금영수증 번호를 입력하지 않은 경우 > 에러 메시지 표시", async () => {
+      fireEvent.click(getCashReciptCheckbox()); // 현금영수증 Check true
+      fireEvent.change(getMessage(), { target: { value: "메시지" } }); // 메시지 입력
+
+      fireEvent.submit(getSubmit());
+
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("현금영수증 번호를 입력해주세요.");
+      });
+    });
+
+    test("현금영수증 번호에 숫자 외의 문자를 입력한 경우 > 에러 메시지 표시", async () => {
+      fireEvent.click(getCashReciptCheckbox()); // 현금영수증 Check true
+      fireEvent.change(getMessage(), { target: { value: "메시지" } }); // 메시지 입력
+      fireEvent.change(getCashReceiptNumber(), { target: { value: "123abc" } }); // 현금 영수증 번호 입력
+
+      fireEvent.submit(getSubmit());
+
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("현금영수증 번호는 숫자로만 입력해주세요.");
+      });
+    });
+
+    test("메시지를 입력하지 않은 경우 > 에러 메시지 표시", async () => {
+      fireEvent.submit(getSubmit());
+
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("메시지를 입력해주세요.");
+      });
+    });
+
+    test("메시지 입력 길이가 100자를 넘을 경우 > 에러 메시지 표시", async () => {
+      fireEvent.change(getMessage(), { target: { value: "0".repeat(101) } });
+      fireEvent.submit(getSubmit());
+
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("메시지는 100자 이내로 입력해주세요.");
+      });
+    });
+
+    test("hasCashReceipt: false > 주문 완료 확인", async () => {
+      fireEvent.change(getMessage(), { target: { value: "메시지" } }); // 메시지 입력
+      fireEvent.submit(getSubmit());
+
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("주문이 완료되었습니다.");
+      });
+    });
+
+    test("hasCashReceipt: true > 주문 완료 확인", async () => {
+      fireEvent.click(getCashReciptCheckbox()); // 현금영수증 Check true
+      fireEvent.change(getMessage(), { target: { value: "메시지" } }); // 메시지 입력
+      fireEvent.change(getCashReceiptNumber(), { target: { value: "12345" } }); // 현금 영수증 번호 입력
+
+      fireEvent.submit(getSubmit());
+
+      await waitFor(() => {
+        expect(window.alert).toHaveBeenCalledWith("주문이 완료되었습니다.");
+      });
     });
   });
 });
