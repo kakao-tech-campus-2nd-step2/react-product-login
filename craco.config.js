247b122a775272478ea80ac7b0cd5d461787bf06
JeongSeoyun <xeonj@naver.com>
2024-07-28 00:37:31 +0900

    chore: jest 설정을 craco에서만 관리하도록 수정

--- craco.config.js
+++ craco.config.js
@@ -6,4 +6,19 @@ module.exports = {
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  jest: {
+    babel: {
+      addPresets: true /* (default value) */,
+      addPlugins: true /* (default value) */,
+    },
+    configure: { /* ... */ },
+    configure: (jestConfig, { env, paths, resolve, rootDir }) => {
+      jestConfig.moduleNameMapper = {
+        ...jestConfig.moduleNameMapper,
+        '^axios$': 'axios/dist/node/axios.cjs',
+      };
+      
+      return jestConfig;
+    },
+  },
 };
