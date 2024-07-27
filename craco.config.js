a1b36c50b24848d765db809289d6a713eeb9e5ea
JeongSeoyun <xeonj@naver.com>
2024-07-28 01:08:21 +0900

    chore: jest의 import 오류 해결을 위해 수정중

--- craco.config.js
+++ craco.config.js
@@ -9,21 +9,26 @@ module.exports = {
   },
   jest: {
     configure: (jestConfig, { env, paths, resolve, rootDir }) => {
-      // Babel을 사용하여 JS 및 TS 파일을 변환하도록 설정
+      // ts-jest를 사용하여 TypeScript 파일을 변환하도록 설정
       jestConfig.transform = {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+        '^.+\\.(ts|tsx)$': 'ts-jest', // TypeScript 파일에 대해 ts-jest를 사용
+        '^.+\\.js$': 'babel-jest', // JavaScript 파일에 대해 babel-jest를 사용
       };
 
-      // 모듈 경로를 매핑
+      // CSS 파일을 모킹하도록 설정
       jestConfig.moduleNameMapper = {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
         '^@/(.*)$': '<rootDir>/src/$1',
         '^@src/(.*)$': '<rootDir>/src/$1',
       };
 
       // transformIgnorePatterns 설정을 추가하여 필요한 모듈에 대해서만 변환을 수행하도록 설정
       jestConfig.transformIgnorePatterns = [
-        '/node_modules/(?!(axios|@tanstack|react-query)/)', // 여기서 필요한 모듈들을 나열
+        '/node_modules/(?!(axios|@tanstack|react-query)/)', // 필요한 모듈에 대해서만 변환을 수행
       ];
+
+      // 테스트 환경 설정
+      jestConfig.testEnvironment = 'jsdom'; // React는 jsdom 환경에서 테스트
 
       return jestConfig;
     },
