9fe1ff652c8d2a19dcaeb2ef72227738432129c5
yunseong0404 <333665@naver.com>
2024-07-27 10:33:28 +0900

    comment: GoodsDetail.test.tsx 주석 추가

--- src/components/__tests__/GoodsDetail.test.tsx
+++ src/components/__tests__/GoodsDetail.test.tsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom/extend-expect';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { fireEvent,render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
 import { OptionSection } from '@/components/features/Goods/Detail/OptionSection';
@@ -25,17 +25,21 @@ test('total amount of payment changed according to quantity', async () => {
     </QueryClientProvider>,
   );
 
-  /// 초기 총 결제 금액 확인
+  //given: 상품상세 페이지
   const totalPrice = await screen.findByText('총 결제 금액');
   expect(totalPrice).toHaveTextContent('145000원');
 
-  // 수량 증가 버튼 클릭
-  const incrementButton = screen.getByLabelText('수량 1개 추가');
+  //when: 수량 증가 버튼을 클릭했을때
+  const incrementButton = await screen.findByLabelText('수량 1개 추가');
   fireEvent.click(incrementButton);
+
+  //then: 총 상품 가격 290000원이 출력된다.
   expect(totalPrice).toHaveTextContent('290000원');
 
-  // 수량 감소 버튼 클릭
-  const decrementButton = screen.getByLabelText('수량 1개 감소');
+  //when: 수량 감소 버튼을 클릭했을때
+  const decrementButton = await screen.findByLabelText('수량 1개 감소');
   fireEvent.click(decrementButton);
+
+  //then: 총 상품 가격 145000원이 출력된다.
   expect(totalPrice).toHaveTextContent('145000원');
 });
