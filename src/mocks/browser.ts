03ee4edbc9e2e582301afd8745ff875300c05bac
seung365 <bdh3659@naver.com>
2024-07-27 14:55:13 +0900

    feat: msw 테스트 서버와 브라우저 서버 설정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -1,6 +1,13 @@
 import { setupWorker } from 'msw';
 
-import { categoriesMockHandler } from '@/api/hooks/categories.mock';
-import { productsMockHandler } from '@/api/hooks/products.mock';
+import { categoriesMockHandler } from '@/api/hooks/Mock/categories.mock';
+import { loginMockHandler } from '@/api/hooks/Mock/login.mock';
+import { productsMockHandler } from '@/api/hooks/Mock/products.mock';
+import { wishListMockHandler } from '@/api/hooks/Mock/wishList.mock';
 
-export const worker = setupWorker(...categoriesMockHandler, ...productsMockHandler);
+export const worker = setupWorker(
+  ...categoriesMockHandler,
+  ...productsMockHandler,
+  ...loginMockHandler,
+  ...wishListMockHandler,
+);
