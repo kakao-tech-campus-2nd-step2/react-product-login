863dcd9f92aaa4e2a352635f68fe8a18f48a4e46
KimJi-An <j0012a@naver.com>
2024-07-27 23:01:42 +0900

    refactor: GoodsDetail.test.tsx의 각 테스트 케이스에서 독립적으로 setup 함수 사용

--- __tests__/GoodsDetail.test.tsx
+++ __tests__/GoodsDetail.test.tsx
@@ -5,16 +5,25 @@ import { MemoryRouter } from "react-router-dom";
 import { GoodsDetail } from "@/components/features/Goods/Detail";
 import { OptionSection } from "@/components/features/Goods/Detail/OptionSection";
 
-const GoodsData = {
-  id: 3245119,
-  name: '[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)',
-  imageUrl: 'https://st.kakaocdn.net/product/gift/product/20240215083306_8e1db057580145829542463a84971ae3.png',
-  price: 145000,
-};
+const setup = (override = {}) => {
+  const GoodsData = {
+    id: 3245119,
+    name: '[단독각인] 피렌체 1221 에디션 오드코롱 50ml (13종 택1)',
+    imageUrl: 'https://st.kakaocdn.net/product/gift/product/20240215083306_8e1db057580145829542463a84971ae3.png',
+    price: 145000,
+  };
 
-const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
+
+  return {
+    GoodsData: { ...GoodsData, ...override },
+    queryClient: queryClient,
+  };
+}
 
 test('상품의 상세 정보를 올바르게 렌더링', async () => {
+  const { GoodsData, queryClient } = setup();
+
   render(
     <QueryClientProvider client={queryClient}>
       <GoodsDetail productId={GoodsData.id.toString()} />
@@ -33,6 +42,8 @@ test('상품의 상세 정보를 올바르게 렌더링', async () => {
 });
 
 test('상품 수량 변경하면 총 결제 금액 업데이트', async () => {
+  const { GoodsData, queryClient } = setup();
+
   render(
     <QueryClientProvider client={queryClient}>
       <MemoryRouter>
@@ -54,6 +65,8 @@ test('상품 수량 변경하면 총 결제 금액 업데이트', async () => {
 });
 
 test('상품 수량의 최소 및 최대 값에 따른 버튼 비활성화', async () => {
+  const { GoodsData, queryClient } = setup();
+
   render(
     <QueryClientProvider client={queryClient}>
       <MemoryRouter>
