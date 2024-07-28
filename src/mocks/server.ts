f9ebdfeee9607107b7fbb2e0d061f51dee805dbe
faddishcorn <juni129461@gmail.com>
2024-07-28 23:04:21 +0900

    chore: mock server 설정

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,7 @@
 import { setupServer } from 'msw/node';
 import { categoriesMockHandler } from '../api/hooks/categories.mock';
 import { productsMockHandler } from '../api/hooks/products.mock';
+import { loginMockHandler } from '../api/hooks/login.mock';
+import { registerMockHandler } from '../api/hooks/register.mock';
 
-export const server = setupServer(...categoriesMockHandler, ...productsMockHandler);
+export const server = setupServer(...categoriesMockHandler, ...productsMockHandler, ...loginMockHandler, ...registerMockHandler);
