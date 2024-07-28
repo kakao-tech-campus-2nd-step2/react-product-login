09924d13538e7ae681327ad8c653ee7250d2701c
faddishcorn <juni129461@gmail.com>
2024-07-29 02:44:18 +0900

    chore: add wishMockHandler to browser and server

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -4,10 +4,12 @@ import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
 import { loginMockHandler } from '@/api/hooks/login.mock';
 import { registerMockHandler } from '@/api/hooks/register.mock';
+import { wishMockHandler } from '../api/hooks/wish.mock';
 
 export const worker = setupWorker(
   ...categoriesMockHandler,
   ...productsMockHandler,
   ...loginMockHandler,
-  ...registerMockHandler
+  ...registerMockHandler,
+  ...wishMockHandler,
 );
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -3,5 +3,12 @@ import { categoriesMockHandler } from '../api/hooks/categories.mock';
 import { productsMockHandler } from '../api/hooks/products.mock';
 import { loginMockHandler } from '../api/hooks/login.mock';
 import { registerMockHandler } from '../api/hooks/register.mock';
+import { wishMockHandler } from '../api/hooks/wish.mock';
 
-export const server = setupServer(...categoriesMockHandler, ...productsMockHandler, ...loginMockHandler, ...registerMockHandler);
+export const server = setupServer(
+  ...categoriesMockHandler, 
+  ...productsMockHandler, 
+  ...loginMockHandler, 
+  ...registerMockHandler,
+  ...wishMockHandler,
+);
