ce446ef7eed2b2e99465870546896e4ed2adaf26
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-07-28 16:13:07 +0900

    feat : browser.ts, server.ts에 wishMockHandles 추가

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -4,9 +4,11 @@ import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
 
 import { userMockHandlers } from '../api/hooks/user.mock';
+import { wishMockHandlers } from '../api/hooks/wish.mock';
 
 export const worker = setupWorker(
   ...categoriesMockHandler,
   ...productsMockHandler,
   ...userMockHandlers,
+  ...wishMockHandlers,
 );
--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,9 +2,11 @@ import { setupServer } from 'msw/node';
 import { categoriesMockHandler } from '../api/hooks/categories.mock';
 import { productsMockHandler } from '../api/hooks/products.mock';
 import { userMockHandlers } from '../api/hooks/user.mock';
+import { wishMockHandlers } from '../api/hooks/wish.mock';
 
 export const server = setupServer(
   ...categoriesMockHandler,
   ...productsMockHandler,
   ...userMockHandlers,
+  ...wishMockHandlers,
 );
