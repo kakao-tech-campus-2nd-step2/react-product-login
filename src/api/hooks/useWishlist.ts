906f50db29504980dd4bc150d0de5546c9030d0b
eunjin <eunjin0321@naver.com>
2024-07-28 12:31:25 +0900

    feat : fetchWishlist 추가

--- src/api/hooks/useWishlist.ts
+++ src/api/hooks/useWishlist.ts
@@ -1,6 +1,5 @@
-import { useMutation, useQueryClient} from '@tanstack/react-query';
-
-import { BASE_URL,fetchInstance } from '../instance';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { BASE_URL, fetchInstance } from '../instance';
 
 interface WishlistRequestParams {
   productId: string;
@@ -14,11 +13,21 @@ export interface WishlistResponseData {
 const WISHLIST_PATH = `${BASE_URL}/api/wishlist`;
 const getWishPath = (wishId: number) => `${BASE_URL}/api/wishes/${wishId}`;
 
+export const fetchWishlist = () =>
+  fetchInstance.get<WishlistResponseData[]>(WISHLIST_PATH).then(res => res.data);
+
 export const postWishlist = (params: WishlistRequestParams) =>
   fetchInstance.post<WishlistResponseData>(WISHLIST_PATH, params).then(res => res.data);
 
 const deleteWishlist = (wishId: number) =>
   fetchInstance.delete<void>(getWishPath(wishId)).then(res => res.status);
+
+export const useFetchWishlist = () => {
+  return useQuery({
+    queryKey: [WISHLIST_PATH],
+    queryFn: fetchWishlist,
+  });
+};
 
 export const usePostWishlist = () => {
   const queryClient = useQueryClient();
