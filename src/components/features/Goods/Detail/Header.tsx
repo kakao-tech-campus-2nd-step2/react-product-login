295709b0599f0f8210d7fa592646e8251dd805f4
joshuadesu <jeongwongwak@naver.com>
2024-07-28 22:08:08 +0900

    Feat: Add Wishlist 추가 시 로그인 상태 확인

--- src/components/features/Goods/Detail/Header.tsx
+++ src/components/features/Goods/Detail/Header.tsx
@@ -1,15 +1,33 @@
 import { Divider } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
 
 import type { ProductDetailRequestParams } from '@/api/hooks/useGetProductDetail';
 import { useGetProductDetail } from '@/api/hooks/useGetProductDetail';
 import { Button } from '@/components/common/Button';
+import { useAuth } from '@/provider/Auth';
+import { getDynamicPath } from '@/routes/path';
 import { breakpoints } from '@/styles/variants';
 
 type Props = ProductDetailRequestParams;
 
 export const GoodsDetailHeader = ({ productId }: Props) => {
   const { data: detail } = useGetProductDetail({ productId });
+  const navigate = useNavigate();
+  const authInfo = useAuth();
+
+  const addWishlist = () => {
+    if (!authInfo) {
+      const isConfirm = window.confirm(
+        '로그인이 필요한 메뉴입니다.\n로그인 페이지로 이동하시겠습니까?',
+      );
+
+      if (!isConfirm) return;
+      return navigate(getDynamicPath.login());
+    } else {
+      alert("관심 목록에 추가되었습니다!");
+    }
+  }
 
   return (
     <Wrapper>
@@ -20,7 +38,7 @@ export const GoodsDetailHeader = ({ productId }: Props) => {
         <Divider color="#f5f5f5" />
         <Notice>카톡 친구가 아니어도 선물 코드로 선물 할 수 있어요!</Notice>
         <Divider color="#f5f5f5" />
-        <Button theme="lightGray" size="large">
+        <Button theme="lightGray" size="large" onClick={addWishlist}>
           관심 리스트에 추가
         </Button>
         <Divider color="#f5f5f5" />
