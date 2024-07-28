3bdda377e8c01205785cd522de34898188668227
joshuadesu <jeongwongwak@naver.com>
2024-07-28 17:17:57 +0900

    Feat: Add 로그인 성공 시 이전 페이지로 돌아가기

--- src/pages/Sign-Up/index.tsx
+++ src/pages/Sign-Up/index.tsx
@@ -1,5 +1,79 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import KAKAO_LOGO from '@/assets/kakao_logo.svg';
+import { Button } from '@/components/common/Button';
+import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
+import { Spacing } from '@/components/common/layouts/Spacing';
+import { breakpoints } from '@/styles/variants';
+import { authSessionStorage } from '@/utils/storage';
+
 export const SignUpPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [queryParams] = useSearchParams();
+
+
+  const handleSignUp = () => {
+    if (!username || !password) {
+      alert('아이디와 비밀번호를 입력해주세요.');
+      return;
+    }
+
+    authSessionStorage.set(username)
+    authSessionStorage.set(password)
+    const redirectUrl = queryParams.get('redirect') ?? `${window.location.origin}/`;
+    return window.location.replace(redirectUrl);
+  }
+
+
   return (
-    <div>사인업</div>
+    <Wrapper>
+      <Logo src={KAKAO_LOGO} alt="카카오 CI" />
+      <FormWrapper>
+        <UnderlineTextField 
+          placeholder="username" 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}  
+        />
+        <Spacing />
+        <UnderlineTextField
+          type="password" 
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Spacing />
+        <Button
+          onClick={handleSignUp}
+        >회원가입</Button>
+      </FormWrapper>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Logo = styled.img`
+  width: 88px;
+  color: #333;
+`;
+
+const FormWrapper = styled.article`
+  width: 100%;
+  max-width: 580px;
+  padding: 16px;
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    padding: 60px 52px;
+  }
+`;
