9a9f89ead048c82e39630e9af6156cb390b4b249
faddishcorn <juni129461@gmail.com>
2024-07-28 23:15:56 +0900

    chore: 회원가입 routing 설정

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -1,3 +1,4 @@
+// src/utils/storage/index.tsx
 import type { OrderHistory } from '@/types';
 
 const initStorage = <T extends keyof StorageKey>(key: T, storage: Storage) => {
@@ -5,26 +6,27 @@ const initStorage = <T extends keyof StorageKey>(key: T, storage: Storage) => {
 
   const get = (): StorageKey[T] => {
     const value = storage.getItem(storageKey);
-
-    return JSON.parse(value as string);
+    return value ? JSON.parse(value) : null;
   };
+
   const set = (value: StorageKey[T]) => {
-    if (value == undefined || value == null) {
+    if (value === undefined || value === null) {
       return storage.removeItem(storageKey);
     }
 
     const stringifiedValue = JSON.stringify(value);
-
     storage.setItem(storageKey, stringifiedValue);
   };
 
   return { get, set };
 };
 
-export const authSessionStorage = initStorage('authToken', sessionStorage);
+export const authSessionStorage = initStorage('authInfo', sessionStorage);
 export const orderHistorySessionStorage = initStorage('orderHistory', sessionStorage);
+export const userSessionStorage = initStorage('user', sessionStorage);
 
 interface StorageKey {
-  authToken?: string;
+  authInfo?: { email: string; token: string };
   orderHistory?: OrderHistory;
+  user? :{email: string; password: string};
 }
