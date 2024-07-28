899a691ccf4839752d73b34b79114ca999c1e3ee
faddishcorn <juni129461@gmail.com>
2024-07-28 23:04:37 +0900

    chore: mock browser 설정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,5 +2,12 @@ import { setupWorker } from 'msw';
 
 import { categoriesMockHandler } from '@/api/hooks/categories.mock';
 import { productsMockHandler } from '@/api/hooks/products.mock';
+import { loginMockHandler } from '@/api/hooks/login.mock';
+import { registerMockHandler } from '@/api/hooks/register.mock';
 
-export const worker = setupWorker(...categoriesMockHandler, ...productsMockHandler);
+export const worker = setupWorker(
+  ...categoriesMockHandler,
+  ...productsMockHandler,
+  ...loginMockHandler,
+  ...registerMockHandler
+);
