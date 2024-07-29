4fc66152a0a2d0d909d9ef41ed8f7b0e5c25d357
dandamdandam <glue0440@gmail.com>
2024-07-29 15:37:27 +0900

    feat: add signin page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled';
+
+import KAKAO_LOGO from '@/assets/kakao_logo.svg';
+import { Button } from '@/components/common/Button';
+import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
+import { Spacing } from '@/components/common/layouts/Spacing';
+import { breakpoints } from '@/styles/variants';
+
+export default () => {
+  return (
+    <Wrapper>
+      <Logo src={KAKAO_LOGO} alt="카카고 CI" />
+      <FormWrapper>
+        <UnderlineTextField placeholder="이름" />
+        <Spacing />
+        <UnderlineTextField type="password" placeholder="비밀번호" />
+
+        <Spacing
+          height={{
+            initial: 40,
+            sm: 60,
+          }}
+        />
+        <Button>회원가입</Button>
+      </FormWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Logo = styled.img`
+  width: 88px;
+  color: #333;
+`;
+
+const FormWrapper = styled.form`
+  width: 100%;
+  max-width: 580px;
+  padding: 16px;
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    padding: 60px 52px;
+  }
+`;
