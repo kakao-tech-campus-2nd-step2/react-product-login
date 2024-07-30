e70f9617bc05ff612c8a5cebfb80dda7122fc3c5
2021048695 <hanyanging@hanyang.ac.kr>
2024-07-30 23:55:48 +0900

    docs: 질문에 대한 답변

--- src/components/common/Button/index.test.tsx
+++ src/components/common/Button/index.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import React from 'react';
 
+// import React from 'react';
 import { Button } from './index';
 
 test('버튼 렌더링과 클릭 테스트', () => {
