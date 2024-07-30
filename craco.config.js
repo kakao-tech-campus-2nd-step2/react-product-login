f6008a415f9037bc671d444469370d9400e714c6
joojjang <itchu999@naver.com>
2024-07-30 12:09:11 +0900

    test(OptionSection): 상품 상세 통합 테스트
    - useGetProductDetail 훅에서 쓰이는 axios를 Jest에서 처리하지 못하는 문제
    - Jest 설정을 수정하고 Babel로 ES -> CommonJS로 변환하도록 했으나 해결 안 됨

--- craco.config.js
+++ craco.config.js
@@ -10,6 +10,15 @@ module.exports = {
     configure: {
       setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
       testEnvironment: 'jsdom',
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1', // 절대 경로 설정
+      },
+      transform: {
+        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Babel을 사용하여 JS/TS 파일을 변환
+      },
+      transformIgnorePatterns: [
+        '/node_modules/(?!(axios)/)', // axios 모듈을 변환 대상으로 포함
+      ],
     },
   },
 };
