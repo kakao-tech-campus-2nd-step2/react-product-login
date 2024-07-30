77a667dee70e5ed793b327fcbbcc30ad1264dc88
joojjang <itchu999@naver.com>
2024-07-31 00:03:21 +0900

    feat(Wish): 헤더에 위시 탭 추가

--- src/components/features/Layout/Header.tsx
+++ src/components/features/Layout/Header.tsx
@@ -9,6 +9,11 @@ export const Header = () => {
   const navigate = useNavigate();
   const authInfo = useAuth();
 
+  const NavButtonList = [
+    { name: '홈', path: RouterPath.home },
+    { name: '위시', path: RouterPath.wish },
+  ];
+
   const handleLogin = () => {
     navigate(getDynamicPath.login());
   };
@@ -16,12 +21,28 @@ export const Header = () => {
   return (
     <Wrapper>
       <Container flexDirection="row" alignItems="center" justifyContent="space-between">
-        <Link to={RouterPath.home}>
-          <Logo
-            src="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png"
-            alt="카카오 선물하기 로고"
-          />
-        </Link>
+        <LogoWrapper>
+          <Link to={RouterPath.home}>
+            <Logo
+              src="https://gift-s.kakaocdn.net/dn/gift/images/m640/pc_gift_logo.png"
+              alt="카카오 선물하기 로고"
+            />
+          </Link>
+        </LogoWrapper>
+        <NavBarWrapper>
+          <NavBar>
+            {NavButtonList.map((button) => (
+              <NavButton key={button.name}>
+                <NavButtonText
+                  to={button.path}
+                  aria-current={location.pathname === button.path ? 'page' : undefined}
+                >
+                  {button.name}
+                </NavButtonText>
+              </NavButton>
+            ))}
+          </NavBar>
+        </NavBarWrapper>
         <RightWrapper>
           {authInfo ? (
             <LinkButton onClick={() => navigate(RouterPath.myAccount)}>내 계정</LinkButton>
@@ -46,10 +67,18 @@ export const Wrapper = styled.header`
   padding: 0 16px;
 `;
 
+const LogoWrapper = styled.div`
+  padding-right: 50px;
+  height: ${HEADER_HEIGHT};
+`;
+
 const Logo = styled.img`
   height: ${HEADER_HEIGHT};
 `;
-const RightWrapper = styled.div``;
+
+const RightWrapper = styled.div`
+  margin-left: auto;
+`;
 
 const LinkButton = styled.p`
   align-items: center;
@@ -57,4 +86,37 @@ const LinkButton = styled.p`
   color: #000;
   text-decoration: none;
   cursor: pointer;
+`;
+
+const NavBarWrapper = styled.nav`
+  padding: 20px 20px 20px 0;
+`;
+
+const NavBar = styled.ul`
+  display: flex;
+  list-style: none;
+`;
+
+const NavButton = styled.li`
+  position: relative;
+  padding-right: 10px;
+  list-style: none;
+`;
+
+const NavButtonText = styled(Link)`
+  display: block;
+  padding: 11px 10px 7px;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 20px;
+
+  &[aria-current='page']:after {
+    position: absolute;
+    left: 8px;
+    right: 16px;
+    bottom: 5px;
+    height: 2px;
+    background-color: #000;
+    content: '';
+  }
 `;
