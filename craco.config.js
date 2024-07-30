b83f2d352660053cde26d27d349a1b47f53eeece
joojjang <itchu999@naver.com>
2024-07-30 13:01:40 +0900

    chore(setup): Jest, Babel 설정 커스텀

--- craco.config.js
+++ craco.config.js
@@ -9,13 +9,16 @@ module.exports = {
   jest: {
     configure: {
       setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+      preset: 'ts-jest',
       testEnvironment: 'jsdom',
       moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1', // 절대 경로 설정
       },
       transform: {
-        '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Babel을 사용하여 JS/TS 파일을 변환
+        '^.+\\.(js|jsx)$': 'babel-jest', // Babel을 사용하여 JS 파일을 변환
+        '^.+\\.(ts|tsx)$': 'ts-jest',
       },
+      moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
       transformIgnorePatterns: [
         '/node_modules/(?!(axios)/)', // axios 모듈을 변환 대상으로 포함
       ],
