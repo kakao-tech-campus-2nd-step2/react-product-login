deddb2a31efe8411486829da604114940cde98af
joojjang <itchu999@naver.com>
2024-07-30 14:50:57 +0900

    feat(SignUp): 회원가입 구현 (api 제외하고)

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,3 +1,90 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+import KAKAO_LOGO from '@/assets/kakao_logo.svg';
+import { Button } from '@/components/common/Button';
+import { UnderlineTextField } from '@/components/common/Form/Input/UnderlineTextField';
+import { Spacing } from '@/components/common/layouts/Spacing';
+import { RouterPath } from '@/routes/path';
+import { breakpoints } from '@/styles/variants';
+import { authSessionStorage } from '@/utils/storage';
+
 export const SignUpPage = () => {
-  return <>회원가입 페이지</>;
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [queryParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate(RouterPath.home);
+  };
+
+  const handleConfirm = async () => {
+    if (!id || !password) {
+      alert('아이디와 비밀번호를 입력해주세요.');
+      return;
+    }
+
+    try {
+      // TODO: 실제 회원가입 API 호출
+
+      // 임시 회원가입 처리
+      authSessionStorage.set(id);
+
+      const redirectUrl = queryParams.get('redirect') ?? RouterPath.home;
+      navigate(redirectUrl);
+    } catch (error) {
+      console.error('failed to sign up:', error);
+      alert('회원가입을 다시 시도해주세요.');
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Logo src={KAKAO_LOGO} alt="카카오 CI" onClick={handleLogoClick} />
+      <FormWrapper>
+        <UnderlineTextField placeholder="이름" value={id} onChange={(e) => setId(e.target.value)} />
+        <Spacing />
+        <UnderlineTextField
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Spacing
+          height={{
+            initial: 40,
+            sm: 60,
+          }}
+        />
+        <Button onClick={handleConfirm}>회원가입</Button>
+      </FormWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Logo = styled.img`
+  width: 88px;
+  color: #333;
+`;
+
+const FormWrapper = styled.article`
+  width: 100%;
+  max-width: 580px;
+  padding: 16px;
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    border: 1px solid rgba(0, 0, 0, 0.12);
+    padding: 60px 52px;
+  }
+`;
